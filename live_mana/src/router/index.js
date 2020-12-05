d3a856645e6e10e69dcd9cd3f9d4d440c45448a0
zepeng <1054521773@qq.com>
2020-12-05 20:05:42 +0800

    11

--- live_mana/src/router/index.js
+++ live_mana/src/router/index.js
@@ -5,19 +5,13 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/',
-        name: 'login',
-        component: () =>
-            import ('@/views/login/login.vue')
-    },
-    {
         path: '/login',
         name: 'login',
         component: () =>
             import ('@/views/login/login.vue')
     },
     {
-        path: '/a',
+        path: '/',
         name: 'Home',
         component: () =>
             import ('@/views/Home.vue'),
